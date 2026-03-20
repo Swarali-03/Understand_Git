de58b19449a8da74abf4ad2a0c536cca219bb1d3
Swarali-03 <swaralideolankar1@gmail.com>
2026-03-20 14:59:14 +0530

    newly updated after gudi padwa

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+#comma separated key value pairs enclosed within {}
+# {key1: value1, key2:value2,...}
+
+groceries={'milk':60, 'biscuits':20, 'rice':30 }
+print(groceries)
+print(type(groceries))
+print(len(groceries))
+print(groceries['milk'])
+groceries['milk']=65 #value changed
+print(groceries)
+groceries['eggs'] = 10
+print(groceries)
+groceries['rice']=50
+print(groceries)
+print('groceries')
